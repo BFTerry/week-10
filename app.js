d6fd602582b3166c1a530e98cd54300c9b2df96d
Terry <bterry@FVFZMY3WJ1WK.tld>
2022-03-24 14:56:29 -0500

    main

--- app.js
+++ app.js
@@ -3,37 +3,41 @@ function askname() {
     return username
 }
 
-function askuom() {
-    var uom = prompt(`Hey ${username} can you tell me how you want your tempature to be converted?`/n` Do so by entering 'f' if you want to convert tempature from Fahrenheit to Celsius, or by entering 'c' if you want to convert tempature from Celsius to Fahrenheit. Any other option will exit the program`);
-    return uom
-}
-
 function tempature() {
     var temp = prompt(`What tempature do you want converted?`)
     return temp
 }
 
-function conversion(um) {
-    if (um == `f`) {
-        var tempc = ((temp - 32) * 5) / 9;
-        
-    } else if (um == `c`) {
-        var tempf = ((temp * 9) / 5) + 32;
-        
+function askuom(usn) {
+    var uom = prompt(`Hey ${usn} can you tell me how you want your tempature to be converted?`/n` Do so by entering 'f' if you want to convert tempature from Fahrenheit to Celsius, or by entering 'c' if you want to convert tempature from Celsius to Fahrenheit. Any other option will exit the program`);
+    return uom
+}
+
+
+
+if (uom == `f`) {
+    function conversionf(tpr) {
+        var ftemp = ((tpr - 32) * 5) / 9;
+        return ftemp
     }
+} else if (uom == `c`) {
+    function conversionc(tpr) {
+        var ftemp = ((tpr * 9) / 5) + 32;
+        return ftemp
+    }
+ }  else {
+        alert(`You needed to say f or c. Goodbye`);
+    }
+    
+
+
+function final(tmr) {
+    alert(`Great you tempature is ${tmr}`);
+
 }
 
-function final(conversion  ) {
-    if ()
-        alert(`Great you tempature is ${tempc}`)
-}
-
-
- 
-function Comverter() {
-
-}
 
 
 
-conversion(askuom(askname()))
+
+final(conversion(askuom(askname()), tempature()));
