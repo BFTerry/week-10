fc4d6d11e7c53e77b745258d431c926af3b68c0a
Terry <bterry@FVFZMY3WJ1WK.tld>
2022-03-25 14:46:33 -0500

    main

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
 function askname() {
     var username = prompt("Hi welcome to the Tempature Comverter, Im T.C.C. the second c is sighlent but it stands for Tempature Converting Computer but call me TC for short. Well i have introduced myself how about you whats your name?");
     return username
-}
+} // 10
 
 function tempature() {
     var temp = prompt(`What tempature do you want converted?`)
     return temp
-}
+} //20
+
 
 function askuom(usn) {
     var uom = prompt(`Hey ${usn} can you tell me how you want your tempature to be converted?`/n` Do so by entering 'f' if you want to convert tempature from Fahrenheit to Celsius, or by entering 'c' if you want to convert tempature from Celsius to Fahrenheit. Any other option will exit the program`);
     return uom
 }
 
+function conversionf(tpr) {
+            var ftemp = ((tpr - 32) * 5) / 9;
+            return ftemp
+        } // 20
 
+function conversionc(tpr) {
+            var ftemp = ((tpr * 9) / 5) + 32;
+            return ftemp
+        } //20
 
-if (uom == `f`) {
-    function conversionf(tpr) {
-        var ftemp = ((tpr - 32) * 5) / 9;
+function conversion(unm) 
+    if (unm == `f`) {
+        
+    } else if (unm == `c`) {
+        
+    }  else {
+            alert(`You needed to say f or c. Goodbye`);
+        }
         return ftemp
-    }
-} else if (uom == `c`) {
-    function conversionc(tpr) {
-        var ftemp = ((tpr * 9) / 5) + 32;
-        return ftemp
-    }
- }  else {
-        alert(`You needed to say f or c. Goodbye`);
-    }
-    
+  
+
+//80
 
 
 function final(tmr) {
@@ -36,8 +43,9 @@ function final(tmr) {
 
 }
 
+askuom(askname())
 
 
 
 
-final(conversion(askuom(askname()), tempature()));
+final(conversion())
